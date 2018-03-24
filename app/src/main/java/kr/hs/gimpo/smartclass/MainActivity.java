d215a56afc457585a9bb0e04ff7a696134affcb9
Users2530 <rhkgkrwk001@outlook.com>
2018-03-24 17:30:19 +0900

    임시 업데이트

--- app/src/main/java/kr/hs/gimpo/smartclass/MainActivity.java
+++ app/src/main/java/kr/hs/gimpo/smartclass/MainActivity.java
@@ -12,6 +12,19 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,19 +39,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /*
-    * initData()
+    * TODO: initData()
     * 1. Firebase DB 연결
-    * 2. 시간표 데이터 업로드
-    * 3. 급식 데이터 업로드
-    * 4. 학사일정 데이터 업로드
-    * 5. 기상 데이터 업로드
+    * 2. 시간표 데이터 업데이트
+    * 3. 급식 데이터 업데이트
+    * 4. 학사일정 데이터 업데이트
+    * 5. 기상 데이터 업데이트
     * 6. 오프라인으로 저장하기
     * 7. 객체 전달하기
     * */
     private void initData() {
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference ref = database.getReference("message");
-        ref.child("asdf").setValue("Hello, world!");
+        DatabaseReference ref = database.getReference("test");
 
         // Read from the database
         ref.addValueEventListener(new ValueEventListener() {
@@ -47,14 +60,66 @@ public class MainActivity extends AppCompatActivity {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
                 String value = dataSnapshot.getValue(String.class);
-                Log.d("message", "Value is: " + value);
+                Log.d("test", "Value is: " + value);
             }
 
             @Override
             public void onCancelled(DatabaseError error) {
                 // Failed to read value
-                Log.w("message", "Failed to read value.", error.toException());
+                Log.w("test", "Failed to read value.", error.toException());
             }
         });
+
+        Calendar calendar = Calendar.getInstance();
+        Date today = Calendar.getInstance().getTime();
+        calendar.setTime(today);
+
+        Document doc;
+
+        // 시간표 데이터 업데이트
+        try {
+            doc = Jsoup.connect("http://comcigan.com:4082/_hourdat?sc=26203").get();
+
+            Element data = doc.body();
+            Log.d("TimeTable", "Data initializing...");
+            JsonParser parser = new JsonParser();
+            JsonElement timeTableData = parser.parse(data.text().trim());
+            TimeTable table = new TimeTable(timeTableData);
+            ref.child("TimeTable").setValue(table);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 급식 데이터 업데이트
+        /*
+        try {
+            for(int i = 1; i < calendar.get(Calendar.DAY_OF_MONTH); i++) {
+
+                calendar.set(Calendar.DAY_OF_MONTH, i);
+                today = new Date(calendar.getTimeInMillis());
+                String[] date = new String[3];
+                date[0] = new SimpleDateFormat("yyyy", Locale.getDefault()).format(today);
+                date[1] = new SimpleDateFormat("MM", Locale.getDefault()).format(today);
+                date[2] = new SimpleDateFormat("dd", Locale.getDefault()).format(today);
+
+                doc = Jsoup.connect("http://www.gimpo.hs.kr/main.php?menugrp=021100&master=meal2&act=list&SearchYear="+date[0]+"&SearchMonth="+date[1]+"&SearchDay="+date[2]+"#diary_list").get();
+                Elements data = doc.select("div.meal_content.col-md-7 div.meal_table table tbody");
+                Log.d("MealInfo", "Data initializing...");
+                for(Element element: data) {
+                    element.text().trim();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 학사일정 데이터 업데이트
+
+        // 기상 데이터 업데이트
+        */
+    }
+    // TODO: 스레드 작성(Jsoup.connect().get()은 메인 스레드에서 작동하지 않는다!
+    class InitDataThread extends Thread {
+
     }
 }
