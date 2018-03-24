bdd6c0491dd14fd2edb36e694c9809efe10db5ed
Users2530 <rhkgkrwk001@outlook.com>
2018-03-24 02:28:10 +0000

    데이터 백업

--- app/src/main/java/kr/hs/gimpo/smartclass/MainActivity.java
+++ app/src/main/java/kr/hs/gimpo/smartclass/MainActivity.java
@@ -3,8 +3,15 @@ package kr.hs.gimpo.smartclass;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +20,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initData();
-    }
-
-    private void initData() {
         Intent intent = new Intent(MainActivity.this, HomeActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    /*
+    * initData()
+    * 1. Firebase DB 연결
+    * 2. 시간표 데이터 업로드
+    * 3. 급식 데이터 업로드
+    * 4. 학사일정 데이터 업로드
+    * 5. 기상 데이터 업로드
+    * 6. 오프라인으로 저장하기
+    * 7. 객체 전달하기
+    * */
+    private void initData() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("message");
+        ref.child("asdf").setValue("Hello, world!");
+
+        // Read from the database
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("message", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("message", "Failed to read value.", error.toException());
+            }
+        });
     }
 }
