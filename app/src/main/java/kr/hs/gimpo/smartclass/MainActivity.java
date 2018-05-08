fd95c1c9a6796d0ccf29c860630d0782d38d0750
Users2530 <rhkgkrwk001@outlook.com>
2018-05-08 15:09:06 +0000

    Added New Activty for SettingActivity

--- app/src/main/java/kr/hs/gimpo/smartclass/MainActivity.java
+++ app/src/main/java/kr/hs/gimpo/smartclass/MainActivity.java
@@ -80,7 +80,7 @@ public class MainActivity extends AppCompatActivity
             }
 
             // Create a new Fragment to be placed in the activity layout
-            Fragment firstFragment = new TimeFragment();
+            Fragment firstFragment = new AirFragment();
 
             // In case this activity was started with special instructions from an
             // Intent, pass the Intent's extras to the fragment as arguments
