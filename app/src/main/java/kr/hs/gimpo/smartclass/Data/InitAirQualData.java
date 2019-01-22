bb4c8c9fd7b16eaf6f9e10f1b2d201516d9b30d6
Users2530 <rhkgkrwk001@outlook.com>
2019-01-22 11:21:57 +0900

    Improve code readability
    - added some comment
    - NOT ANYTHING BUT JUST CLEANED SOURCE CODE

--- app/src/main/java/kr/hs/gimpo/smartclass/Data/InitAirQualData.java
+++ app/src/main/java/kr/hs/gimpo/smartclass/Data/InitAirQualData.java
@@ -20,18 +20,20 @@ public class InitAirQualData
         extends AsyncTask<Void, Void, Boolean> {
 
     private String[] airData = new String[15];
-    private final String[] airDataFormat = {"dataTime", "pm10", "pm25", "o3", "no2", "co", "so2", "khai"};
+    private final String[] airDataFormat =
+    {"dataTime", "pm10", "pm25", "o3", "no2", "co", "so2", "khai"};
     private DatabaseReference mDatabase;
-    private boolean isUpdateNeed;
 
-    public InitAirQualData(DatabaseReference mDatabase, @NonNull String thisTime) {
+    // this constructor is no longer supported and will be deprecated.
+    // please call function InitAirQualData()
+    public InitAirQualData(
+      DatabaseReference mDatabase,
+      @NonNull String thisTime
+    ) {
         this.mDatabase = mDatabase;
-
-        this.isUpdateNeed = (thisTime.compareTo(new SimpleDateFormat("yyyy-MM-dd HH", Locale.getDefault()).format(Calendar.getInstance().getTime())) != 0);
     }
 
     public InitAirQualData() {
-        this.isUpdateNeed = true;
         this.mDatabase = FirebaseDatabase.getInstance().getReference("test");
     }
 
@@ -46,9 +48,12 @@ public class InitAirQualData
       int m = Integer.parseInt(sdf.format(Calendar.getInstance().getTime()));
 
       try{
-          // API와 연결합니다.
-          Document doc = Jsoup.connect("http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey="+
-                  "uTRaH16OBrv%2BrnhI1l%2BhctIkvNd6DwX%2FxpnCRXHGHLjpRpVqxmQJ7Q4cXR0wucoc%2Bx3v8hg%2BsVZRvhPTzXS1xw%3D%3D"+
+          // Trying to connect with API and to get raw data
+          Document doc = Jsoup.connect("http://openapi.airkorea.or.kr/" +
+                  "openapi/services/rest/ArpltnInforInqireSvc/" +
+                  "getMsrstnAcctoRltmMesureDnsty?serviceKey="+
+                  "uTRaH16OBrv%2BrnhI1l%2BhctIkvNd6DwX%2FxpnCRXHGHL"+ // \
+                  "jpRpVqxmQJ7Q4cXR0wucoc%2Bx3v8hg%2BsVZRvhPTzXS1xw%3D%3D"+
                   "&numOfRows=1&" +
                   "pageSize=1&" +
                   "pageNo=1&" +
@@ -57,39 +62,47 @@ public class InitAirQualData
                   "dataTerm=DAILY&" +
                   "ver=1.3").get();
 
-          // 반환되는 데이터의 무결성을 검사합니다.
+          // Checking data
           String resultCode = doc.getElementsByTag("resultCode").text().trim();
 
           Log.i("InitAirQualData", "resultCode: " + resultCode);
-          // 결과코드가 00일 때 정상적으로 데이터를 받아온 것입니다.
+          // the data can be analyzed with the result code 00.
           if(resultCode.compareTo("00") == 0) {
               int cnt = 0;
               for(String e : airDataFormat) {
                   if(cnt != 0) {
-                      airData[cnt] = doc.getElementsByTag(e + "Value").text().trim();
-                      airData[cnt+7] = doc.getElementsByTag(e + "Grade").text().trim();
-                      airData[cnt+7] = airData[cnt+7].compareTo("") != 0? airData[cnt+7]:"-1";
+                      airData[cnt] =
+                        doc.getElementsByTag(e + "Value").text().trim();
+                      airData[cnt+7] =
+                        doc.getElementsByTag(e + "Grade").text().trim();
+                      airData[cnt+7] =
+                        airData[cnt+7].compareTo("") != 0? airData[cnt+7]:"-1";
                   } else {
-                      airData[cnt] = doc.getElementsByTag(e).text().trim();
+                      airData[cnt] =
+                        doc.getElementsByTag(e).text().trim();
                   }
                   System.out.println(airData[cnt]);
                   cnt++;
               }
 
-              // 데이터를 Firebase DB에 업데이트합니다.
-              DataFormat.airQualDataFormat = new DataFormat.AirQual(Arrays.asList(airData));
+              // updating data to the Firebase Database.
+              DataFormat.airQualDataFormat =
+                new DataFormat.AirQual(Arrays.asList(airData));
 
-              mDatabase.child("airQualDataFormat").setValue(DataFormat.airQualDataFormat);
+              mDatabase.child("airQualDataFormat")
+                .setValue(DataFormat.airQualDataFormat);
 
-              // 성공 로그를 표시합니다.
-              Log.i("InitAirQualData", "Initialization Completed Successfully.");
+              // printing info to the console
+              Log.i("InitAirQualData",
+                "Initialization Completed Successfully.");
           } else {
-              // 결과코드가 00이 아니면 데이터를 받아오지 못한 것입니다.
-              String resultMessage = doc.getElementsByTag("resultMsg").text().trim();
+              // the data cannot be analyzed without the result code 00.
+              String resultMessage =
+                doc.getElementsByTag("resultMsg").text().trim();
 
               Log.e("InitAirQualData", "resultMsg: " + resultMessage);
 
-              // 실패 로그를 표시합니다.
+              // printing infor to the console
               Log.e("InitAirQualData", "Initialization Failed!");
 
               return false;
